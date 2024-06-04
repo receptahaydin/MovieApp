f547e2d6adef5d313f3a8c217ea28f8f1d220ed8
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-06-04 10:29:49 +0300

    For the label color on dark and light mode, Color+Extensions created

---
+++ MovieApp/Extensions/Color+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Extensions.swift
+//  MovieApp
+//
+//  Created by Recep Taha Aydın on 4.06.2024.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    // MARK: - Label Colors
+    static let label = Color(UIColor.label)
+}
