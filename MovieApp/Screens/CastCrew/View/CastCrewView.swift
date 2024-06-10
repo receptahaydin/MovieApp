e32fe36ba8b0751f677c34f25cf9759fee52bdae
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-06-10 17:23:16 +0300

    CastCrewView and PhotosView created, images and casts arrays given from the MovieDetailView

---
+++ MovieApp/Screens/CastCrew/View/CastCrewView.swift
@@ -0,0 +1,28 @@
+//
+//  CastCrewView.swift
+//  MovieApp
+//
+//  Created by Recep Taha Aydın on 9.06.2024.
+//
+
+import SwiftUI
+
+struct CastCrewView: View {
+    let casts: [Cast]
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 15) {
+                ForEach(casts, id: \.self) { cast in
+                    CastCrewCell(cast: cast)
+                }
+            }
+            .padding()
+        }
+        .navigationTitle("Cast & Crew")
+    }
+}
+
+#Preview {
+    CastCrewView(casts: [])
+}
---
+++ MovieApp/Screens/Photos/View/PhotosView.swift
@@ -0,0 +1,46 @@
+//
+//  PhotosView.swift
+//  MovieApp
+//
+//  Created by Recep Taha Aydın on 10.06.2024.
+//
+
+import SwiftUI
+
+struct PhotosView: View {
+    var images: [Backdrop] = []
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 15) {
+                ForEach(images, id: \.self) { image in
+                    if let imagePath = image.filePath, let url = URL(string: "http://image.tmdb.org/t/p/w500\(imagePath)") {
+                        AsyncImage(url: url) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: .infinity)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        } placeholder: {
+                            Image(systemName: "photo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: .infinity)
+                        }
+                    } else {
+                        Image(systemName: "photo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxWidth: .infinity)
+                    }
+                }
+            }
+            .padding()
+        }
+        .navigationTitle("Photos")
+    }
+}
+
+#Preview {
+    PhotosView()
+}
