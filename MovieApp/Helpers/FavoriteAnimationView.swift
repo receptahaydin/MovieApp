104db248aa0bd71cc09da2d1d4287891a8c81ccf
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-06-13 12:33:46 +0300

    FavoriteAnimationView updated according to favorite status

--- MovieApp/Helpers/FavoriteAnimationView.swift
+++ MovieApp/Helpers/FavoriteAnimationView.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct FavoriteAnimationView: View {
+    @Environment(\.modelContext) var context
+    @Query var movies: [Movie] = []
     @State private var opacity: CGFloat = 0.0
     @State private var scaleHeart: CGFloat = 0.0
     @State private var scaleText: CGFloat = 1
     @Binding var isLiked: Bool
+    var movieID: Int
     
     var body: some View {
         ZStack {
@@ -26,18 +30,22 @@ struct FavoriteAnimationView: View {
                     
                     Image(systemName: "heart.fill")
                         .foregroundColor(.red)
-                        .scaleEffect(scaleHeart)
+                        .scaleEffect(isLiked ? 1.0 : 0.0)
                         .animation(Animation.interpolatingSpring(stiffness: 170, damping: 10).delay(0.5))
                 }
                 .onTapGesture {
                     isLiked.toggle()
                     opacity += 1
-                    scaleHeart += 1
-                    scaleText = 1.2
-                    if !isLiked {
-                        scaleHeart = 0
-                        isLiked = false
-                        scaleText = 1
+                    scaleHeart = isLiked ? 1.0 : 0.0
+                    scaleText = isLiked ? 1.2 : 1.0
+                    
+                    if isLiked {
+                        let movie = Movie(id: movieID)
+                        context.insert(movie)
+                    } else {
+                        if let index = movies.firstIndex(where: { $0.id == movieID }) {
+                            context.delete(movies[index])
+                        }
                     }
                 }
             }
@@ -46,5 +54,5 @@ struct FavoriteAnimationView: View {
 }
 
 #Preview {
-    FavoriteAnimationView(isLiked: .constant(true))
+    FavoriteAnimationView(isLiked: .constant(true), movieID: 0)
 }
