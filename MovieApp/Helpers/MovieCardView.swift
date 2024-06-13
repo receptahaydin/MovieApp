6a7c05bb22787dc09c614278237dcb6e1cdeaaf8
recepaydin <receptaha.aydin@stu.fsm.edu.tr>
2024-06-13 14:58:15 +0300

    NavigationLink added to MovieCardView for refactoring

--- MovieApp/Helpers/MovieCardView.swift
+++ MovieApp/Helpers/MovieCardView.swift
@@ -7,60 +7,63 @@
 
 import SwiftUI
 import Kingfisher
+import SwiftData
 
 struct MovieCardView: View {
     
-    let movie: TopRatedMovie
+    @Query var movies: [Movie]
     let details: MovieDetails?
     let certification: String?
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 5) {
-            if let url = URL(string: "http://image.tmdb.org/t/p/w500\(movie.posterPath)") {
-                KFImage(url)
-                    .placeholder {
-                        ProgressView()
-                    }
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 250)
-            }
-            
-            HStack(spacing: 2) {
-                StarsRatingView(rating: movie.voteAverage / 2)
-            }
-            .padding(.top, 5)
-            
-            Text(movie.title)
-                .font(.callout)
-                .fontWeight(.medium)
-                .lineLimit(1)
-                .foregroundStyle(Color.label)
-            
-            HStack(alignment: .bottom, spacing: 4) {
-                Text("\(details?.genres?.first?.name ?? "")")
-                
-                if let runtime = details?.runtime?.toHoursMinutes(), !runtime.isEmpty {
-                    Text("• \(runtime)")
+        NavigationLink(destination: MovieDetailView(movieID: details?.id ?? 0, details: details, movies: movies)) {
+            VStack(alignment: .leading, spacing: 5) {
+                if let url = URL(string: "http://image.tmdb.org/t/p/w500\(details?.posterPath ?? "")") {
+                    KFImage(url)
+                        .placeholder {
+                            ProgressView()
+                        }
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 250)
                 }
                 
-                if let certification = certification, !certification.isEmpty {
-                    Text("|")
-                        .font(.system(size: 13))
+                HStack(spacing: 2) {
+                    StarsRatingView(rating: (details?.voteAverage ?? 0) / 2)
+                }
+                .padding(.top, 5)
+                
+                Text(details?.title ?? "")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .lineLimit(1)
+                    .foregroundStyle(Color.label)
+                
+                HStack(alignment: .bottom, spacing: 4) {
+                    Text("\(details?.genres?.first?.name ?? "")")
                     
-                    Text(certification)
+                    if let runtime = details?.runtime?.toHoursMinutes(), !runtime.isEmpty {
+                        Text("• \(runtime)")
+                    }
+                    
+                    if let certification = certification, !certification.isEmpty {
+                        Text("|")
+                            .font(.system(size: 13))
+                        
+                        Text(certification)
+                    }
                 }
+                .lineLimit(1)
+                .font(.system(size: 12))
+                .fontWeight(.medium)
+                .foregroundStyle(.gray)
             }
-            .lineLimit(1)
-            .font(.system(size: 12))
-            .fontWeight(.medium)
-            .foregroundStyle(.gray)
+            .frame(width: 164)
+            .padding()
         }
-        .frame(width: 164)
-        .padding()
     }
 }
 
 #Preview {
-    MovieCardView(movie: TopRatedMovie(id: 0, posterPath: "", title: "", voteAverage: 1, overview: ""), details: MovieDetails(genres: [], runtime: 0), certification: "")
+    MovieCardView(details: MovieDetails(id: 0, genres: [], overview: "", posterPath: "", runtime: 0, title: "", voteAverage: 0), certification: "")
 }
